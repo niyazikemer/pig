fd37ded3f4d78b7cd4943919333f8b38fa02a2df
niyazikemer <nkemer@gmail.com>
2026-08-03 01:13:55 +0300

    args now available as list items

--- src/pig_coding/cli.zig
+++ src/pig_coding/cli.zig
@@ -1,6 +1,7 @@
-
+const build_options = @import("build_options");
 const std = @import("std");
 
-pub fn run(init:std.process.Init) !void {
-
-}
+pub fn run(init: std.process.Init) !void {
+    const args = try init.minimal.args.toSlice(init.arena.allocator());
+    _ = args;
+} 
